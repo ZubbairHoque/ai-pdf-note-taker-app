22eceba297ca1a21f4555f8bb252ba6c295a82bf
Zubbair <zubbairhoque@gmail.com>
2025-03-22 16:37:34 +0000

    updates

--- app/dashboard/_components/sidebar.tsx
+++ app/dashboard/_components/sidebar.tsx
@@ -1,9 +1,23 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 import React from 'react'
 
 function Sidebar() {
   return (
-    <div>
-      
+
+    /* sidebar header*/  
+
+    <div className='shadow-md h-screen p-3'>
+      <Image src={"/logo.PNG"} alt ="logo" width={75} height={45}/>
+      <div className='mt-5'>
+        <Button className="w-full">+ Upload PDF</Button>
+      </div>
+
+    {/* sidebar options */}
+
+      <div>
+        
+      </div>
     </div>
   )
 }

059cfc7fa40b96008b9e5d64910462dd87bad756
Zubbair <zubbairhoque@gmail.com>
2025-03-23 15:03:02 +0000

    designing header component

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import Sidebar from "./_components/sidebar";
+import Header from "./_components/Header";
 
 
 function DashboardLayout({
@@ -14,7 +15,10 @@ function DashboardLayout({
         <Sidebar />
       </div>
       <div className="md:ml-64">
-        {children}
+        <Header />
+        <div className="p-10">
+          {children}
+        </div>
       </div>
     </div>
   )

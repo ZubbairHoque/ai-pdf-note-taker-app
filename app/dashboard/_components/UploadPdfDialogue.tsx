42285ada23f81633dadf4f40017adc681b863d99
Zubbair <zubbairhoque@gmail.com>
2025-03-24 17:23:33 +0000

    updates

--- app/dashboard/_components/UploadPdfDialogue.tsx
+++ app/dashboard/_components/UploadPdfDialogue.tsx
@@ -1,29 +1,39 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { DialogClose } from "@radix-ui/react-dialog";
 import { Input } from "@/components/ui/input";
+import { api } from "@/convex/_generated/api";
 
 import React, { useRef, useState } from "react";
+import { useMutation } from "convex/react";
 
 function UploadPdfDialogue({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
+  // Generating a URL and uploading it to convex storage
+  const generateUploadUrl = useMutation(api.fileStorage.generateUploadUrl);
+  const savePdfFile = useMutation(api.fileStorage.savePdfFile);
+  
+
+  // JavaScript logic for handling file input
   const fileInputRef = useRef<HTMLInputElement | null>(null);
   const [fileName, setFileName] = useState<string>("No file chosen");
   const [customFileName, setCustomFileName] = useState<string>("");
+  const [loading, setloading ] = useState<boolean>(false);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
+     const file = event.target.files?.[0];
     setFileName(file ? file.name : "No file chosen");
     setCustomFileName(file ? file.name : ""); // Default the input field to the file name
   };
@@ -34,6 +44,34 @@ function UploadPdfDialogue({
     }
   };
 
+  const OnUpload= async ()=>{
+    setloading(true);
+
+    // step 1: get short-lived upload url
+    const postUrl = await generateUploadUrl();
+
+    // Step 2: POST the file to the URL
+    const file = fileInputRef.current?.files?.[0];
+    if (!file) {
+      console.error("No file selected");
+      setloading(false);
+      return;
+    }
+
+    const result = await fetch(postUrl, {
+      method: "POST",
+      headers: { "Content-Type": file.type },
+      body: file,
+    });
+    const { storageId } = await result.json();
+    console.log('storageId', storageId);
+    consr fileID = uuid4()
+
+    setloading(false);
+
+     // Step 3: Save the newly allocated storage id to the database
+
+  }
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
@@ -95,7 +133,16 @@ function UploadPdfDialogue({
                 </DialogClose>
 
                 {/* Upload Button */}
-                <Button className="bg-blue-500 hover:bg-blue-700 text-white">
+                <Button className="bg-blue-500 hover:bg-blue-700 text-white
+                flex items-center">
+                  {loading?
+                    <svg
+                      className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                      xmlns="http://www.w.WebSockets.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    ></svg>:'Upload'
+                  }
                   Upload
                 </Button>
               </div>

4a4ffe652d367cf40244432b15bd2edfab42ad7d
Zubbair <zubbairhoque@gmail.com>
2025-06-02 23:18:09 +0100

    updates

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,35 @@
-import { SignUp } from '@clerk/nextjs'
+import { SignUp, useUser } from '@clerk/nextjs'
+import { useMutation } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { useEffect } from 'react';
 
 export default function Page() {
-  return <SignUp />
+  const { user } = useUser();
+  const createUser = useMutation(api.user.createUser);
+
+  useEffect(() => {
+    if (
+      user &&
+      user.primaryEmailAddress?.emailAddress &&
+      user.fullName &&
+      user.imageUrl
+    ) {
+      createUser({
+        email: user.primaryEmailAddress.emailAddress,
+        userName: user.fullName,
+        imageUrl: user.imageUrl,
+      });
+    }
+  }, [user]);
+
+  return (
+    <div className="
+    flex 
+    justify-center 
+    items-center 
+    h-screen"
+    >
+      <SignUp />
+    </div>
+  );
 }
